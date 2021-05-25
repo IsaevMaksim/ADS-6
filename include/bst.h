8ad2475364b154e6e5330cea9cc33f2221456fe2
IsaevMaksim <78897852+IsaevMaksim@users.noreply.github.com>
2021-05-25 13:25:22 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -11,7 +11,7 @@ class BST {
     Node * left;
     Node * right;
   };
- 
+
  private:
   Node * root;
   Node * addNode(Node *root, T value) {
@@ -51,7 +51,7 @@ class BST {
         return searchNode(root->left, value);
     }
   }
-  
+
  public:
   BST() : root(nullptr) {}
   ~BST() {}
